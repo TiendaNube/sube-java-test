1e1982e01a68909d06a1f7483a3c83b6444110eb
zeta <jzelayeta@tiendanube.com>
2020-07-07 16:47:32 -0300

    scala project

---
+++ src/main/scala/Sube.scala
@@ -0,0 +1,33 @@
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+/**
+ * Modelar el uso de la tarjeta Sube, para los siguientes casos de uso:
+ * + Revisar la implementación actual y hacer los cambios que crea necesario.
+ * + Agregar fondos a la tarjeta. Si el saldo actual es menor que cero el monto mínimo a cargar debe ser de 50 pesos.
+ * Ejemplo: si tengo -20 pesos, el monto mínimo a cargar debe ser 50 pesos.
+ * + Registrar un nuevo viaje y descontar el saldo, teniendo en cuenta que:
+ *      - Se aplicará un descuento del 10% si el tiempo transcurrido entre el viaje actual y el último es menor a una hora.
+ *      - Si el saldo resultante es menor a -50 pesos no se podrá realizar el viaje.
+ * + Se bonificará al usuriao con un 15% de descuento sobre lo consumido en el mes anterior, si el promedio diario
+ * en días habiles fue de cinco viajes.
+ * Emjemplo: durante el mes de Marzo todos los dias habiles un usuario hace cinco viajes de 20 pesos cada uno.
+ * Asumiendo que hay 20 dias habiles se lo bonificará con 300 pesos.
+ */
+object Sube {
+  def main(args: Array[String]): Unit = {
+  }
+}
+
+class Sube(var cardId: String) {
+  var amount: BigDecimal = BigDecimal.ZERO
+  var lastTrip: LocalDateTime = null
+
+  def addFounds(amount: BigDecimal): Unit = {
+    this.amount = this.amount.add(amount)
+  }
+
+  def travel(amount: BigDecimal): Unit = {
+    this.amount = this.amount.subtract(amount)
+  }
+}
---
+++ src/test/scala/SubeSpec.scala
@@ -0,0 +1,12 @@
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class SubeSpec extends AnyWordSpecLike with Matchers {
+
+  "sube" should {
+    "dummy" in {
+      1 shouldBe 1
+    }
+  }
+
+}
